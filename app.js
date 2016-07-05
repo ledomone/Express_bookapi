aa4d86927cbef8355bfa9c7bac4610c2d5320158
Dominik <ledom.git@gmail.com>
2016-07-05 15:39:55 +0200

    Code Cleanup: Routes/bookRoutes

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
-var Book = require('./models/bookModel');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -11,41 +10,7 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
-
-bookRouter.route('/Books')
-  .post(function(req, res) {
-    var book = new Book(req.body);
-
-    book.save();
-    res.status(201).send(book);
-  })
-  .get(function(req, res){
-
-    var query = {};
-
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, function(err, books) {
-      if (err)
-        res.status(500).send(err);
-      else {
-        res.json(books);
-      }
-    });
-  });
-
-bookRouter.route('/Books/:bookId')
-  .get(function(req, res) {
-    Book.findById(req.params.bookId, function(err, book) {
-      if (err)
-        res.status(500).send(err);
-      else {
-        res.json(book);
-      }
-    });
-  });
+bookRouter = require('./Routes/bookRoutes')();
 
 app.use('/api', bookRouter);
 
