fe89c4c4bdb41e4876364b1a54381e73fcc8e025
Dominik <ledom.git@gmail.com>
2016-07-05 15:31:57 +0200

    Saving Data

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ bookRouter.route('/Books')
   .post(function(req, res) {
     var book = new Book(req.body);
 
-    console.log(book);
-    res.send(book);
+    book.save();
+    res.status(201).send(book);
   })
   .get(function(req, res){
 
