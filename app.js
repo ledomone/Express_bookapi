7c93b0d22102024b3ff05a5fae20cef5bee798d9
Dominik <ledom.git@gmail.com>
2016-07-05 10:14:02 +0200

    Setting environment with Gulp set up

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.send('welcome to my super API!');
+});
+
+app.listen(port, function(){
+  console.log('Running on PORT: ' + port);
+});
